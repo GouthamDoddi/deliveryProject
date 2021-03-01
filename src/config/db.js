0b0aa8c533ff3489fd3a25c95bb8562af258b7e7
goutham Doddi <gouthambolt@gmail.com>
2021-03-01 14:47:23 +0530

    added getpackagebyId

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'eyutqxfmgrdeqr',
     password: 'c7fd7baea32d716e9bd61369155d3df057dd9acd4ea342b82b8310ee4130386e',
-    host: 'd1qoulkt024fo0',
+    host: 'ec2-18-215-99-63.compute-1.amazonaws.com',
     port: 5432,
     database: 'd1qoulkt024fo0',
 });
