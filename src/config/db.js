a3e330df28d3d6a1eb09d062ced4db942fec1ab2
goutham Doddi <gouthambolt@gmail.com>
2021-03-14 16:15:23 +0530

    1

--- src/config/db.js
+++ src/config/db.js
@@ -3,10 +3,10 @@ const { Pool } = require('pg');
 const pool = new Pool({
     user: 'eyutqxfmgrdeqr',
     password: 'c7fd7baea32d716e9bd61369155d3df057dd9acd4ea342b82b8310ee4130386e',
-    host: '54.234.205.135',
+    host: 'ec2-18-215-99-63.compute-1.amazonaws.com',
     port: 5432,
     database: 'd1qoulkt024fo0',
-    // ssl: true,
+    ssl: true,
 });
 
 // const pool = new Pool({
