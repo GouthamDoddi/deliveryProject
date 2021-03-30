29e86cd24aa7c5f3145e7b5aae8c6c8e9614fa33
goutham Doddi <gouthambolt@gmail.com>
2021-03-30 13:22:22 +0530

    fixed login

--- src/controllers/sendSms.js
+++ src/controllers/sendSms.js
@@ -47,13 +47,40 @@ const sendSMS = async (req, res) => {
 
         console.log(`result3 = ${result3.rowCount}`);
 
-        if (!result3) {
+        if (!result3 && !result1.rowCount) {
             console.log('No user found');
             logger.info(`${req.body.mobileNum} does not exist in either
                 customer or truckowner database`);
 
             return res.json({ statusCode: 404,
                 message: 'Error! User is not found.' });
+        } else if (!result3 && result1.rowCount) {
+            const otp = createOTP();
+            const token = jwt.sign({ sub: req.body.mobileNum }, secret, {
+                expiresIn: 86400, // expires in 24 hours
+            });
+
+            return res.json({
+                statusCode: 200,
+                customerDetails: result1.rows,
+                otp,
+                token,
+                ipAddress: parseIp(req),
+            });
+        } else if (result3 && !result1.rowCount) {
+            const otp = createOTP();
+            const token = jwt.sign({ sub: req.body.mobileNum }, secret, {
+                expiresIn: 86400, // expires in 24 hours
+            });
+
+            return res.json({
+                statusCode: 200,
+                transportOwner: result3[0],
+                truckDetails: result3[1],
+                otp,
+                token,
+                ipAddress: parseIp(req),
+            });
         }
         const otp = createOTP();
         const token = jwt.sign({ sub: req.body.mobileNum }, secret, {
@@ -85,10 +112,20 @@ const sendSMS = async (req, res) => {
 
     // console.log('result2');
     // console.log(result2);
+    if (result1.rowCount) {
+        return res.json({
+            statusCode: 200,
+            customerDetails: result1.rows,
+            truckOwner: result2[0],
+            truckDetails: result2[1],
+            otp,
+            token,
+            ipAddress: parseIp(req),
+        });
+    }
 
     return res.json({
         statusCode: 200,
-        customerDetails: result1.rows,
         truckOwner: result2[0],
         truckDetails: result2[1],
         otp,
